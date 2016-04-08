e14a326cb6d155eff99bf3a0babb8d8b11a27f09
Matt Lyons <matt@matt-lyons.net>
2016-04-08 15:32:40 -0700

    Implement Start Menu UI

--- src/dicegame/DiceGame.java
+++ src/dicegame/DiceGame.java
@@ -1,11 +1,26 @@
 package dicegame;
 
+import javax.swing.UIManager;
+
 /**
  * Main class, creates the GUI
  * @author Matt Lyons, David Lukacs, Daniel Kercheski, David McClure
  */
 public class DiceGame {
-	public static void main(String[] args){
-		new GUI();
+    public static void main(String[] args){
+            
+            //Use operating system look and feel.
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            if (UIManager.getSystemLookAndFeelClassName().equals("javax.swing.plaf.metal.MetalLookAndFeel")) { //Fix detection with linux
+                if (System.getProperty("os.name").contains("Linux")) {
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        GUI gui = new GUI();
 	}
 }
--- src/dicegame/GUI.java
+++ src/dicegame/GUI.java
@@ -2,17 +2,23 @@ package dicegame;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  * Handle the Creation of the UI here
  * @author Matt Lyons, David Lukacs, Daniel Kercheski, David McClure
  */
-public class GUI extends JFrame{
+public class GUI extends JFrame implements ActionListener{
     
     private JButton startButton;
+    private JButton createPlayerButton;
     private JButton quitButton; 
+    private JLabel playersLabel;
     
     public GUI(){
         super("Dice Golf");
@@ -23,15 +29,28 @@ public class GUI extends JFrame{
     public void initLayout(){
         setLayout(new GridBagLayout());
         GridBagConstraints cons = new GridBagConstraints();
-        cons.gridx=0; cons.gridy=0; //Use Grid cell 0,0
         
         startButton = new JButton("Start Game");
+        startButton.addActionListener(this);
+        cons.gridx=0; cons.gridy=0; //Use Grid cell 0,0
+        cons.weightx = 100; cons.weighty = 1;
+        cons.insets = new Insets(5,5,5,5); //Add 5px all around of padding
         add(startButton,cons);
         
-        cons.gridy=1;
+        createPlayerButton = new JButton("Create New Player");
+        createPlayerButton.addActionListener(this);
+        cons.gridy++;
+        add(createPlayerButton,cons);
+        
+        cons.gridy++;
         quitButton = new JButton("Quit Game"); 
+        quitButton.addActionListener(this);
         add(quitButton,cons);
         
+        playersLabel = new JLabel("No Players Created");
+        cons.gridy++; cons.anchor = GridBagConstraints.SOUTH;
+        cons.weighty = 100;
+        add(playersLabel,cons);
     }
     
     public void initGui(){
@@ -39,5 +58,16 @@ public class GUI extends JFrame{
         
         setSize(300,500);
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if(event.getSource() == startButton){
+            //Start Game
+        } else if(event.getSource() == createPlayerButton){
+            //Create Player
+        } else if(event.getSource() == quitButton){
+            System.exit(0);
+        }
     }
 }
