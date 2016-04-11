f7ada1c7c23bd94eff44da2c9fcdf222f76b90ec
David Lukacs <dlukacs@asu.edu>
2016-04-10 22:27:47 -0700

    PlayerCreationPanel class
    Containing basic layout and operations. Further implements needs to
    revert back to StartMenuPanel class.

--- src/dicegame/UI/PlayerCreationPanel.java
+++ src/dicegame/UI/PlayerCreationPanel.java
@@ -2,9 +2,11 @@ package dicegame.UI;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JPanel;
+
+import javax.swing.*; 
 
 /**
  *
@@ -13,25 +15,70 @@ import javax.swing.JPanel;
 public class PlayerCreationPanel extends JPanel implements ActionListener{
     
     private GUI gui;
+    private JLabel playersName; 
+    private JTextField enterName;
+    private JTextField displayName; 
+    private JButton addPlayer; 
+    private String name; 
     
     public PlayerCreationPanel(GUI gui){
         super();
         
-        this.gui=gui;
+        this.gui = gui;
+        
         
         setLayout(new GridBagLayout());
-        GridBagConstraints cons = new GridBagConstraints();
+        GridBagConstraints cons = new GridBagConstraints();        
+        cons.gridx = 0;
+        cons.gridy = 0;
+        cons.weightx = 1;
+        cons.weighty = 30;
+        cons.fill = GridBagConstraints.BOTH; //Stretch components to fill space
+        cons.insets = new Insets(5, 5, 5, 5); //Add some padding to components
+
         
         //Add JLabel
+        playersName = new JLabel ("Enter Players Name");
+        add(playersName,cons);
+        
+    
         
         //Add JTextField (use getText() to get entered text)
-        //You can update the players label by using setText("new text") on the label object
+        cons.gridy++;
+        enterName = new JTextField();
+        enterName.getText();
+        add(enterName,cons);
         
+        //You can update the players label by using setText("new text") on the label object
         //Add Create Button (remember to add action listener)
+        cons.gridy++;
+        addPlayer = new JButton("add");
+        addPlayer.addActionListener(this);
+        add(addPlayer, cons);
+        
+        //Adding JTextField to display name for entered players
+        cons.gridy++;
+        displayName = new JTextField();
+        displayName.setText(name);
+        add(displayName, cons);
+        
     }
     
     @Override
     public void actionPerformed(ActionEvent event) {
+        
+        //Storing player name and exiting back to start menu
+        if(event.getSource() == addPlayer){
+        	
+        	//storing name
+        	name = enterName.getText();
+        	displayName.setText(name);
+        	name = name + " , "; 
+        	
+        	//removing panel
+        	gui.removeAll();
+        	gui.repaint();
+        }
         
     }
     
