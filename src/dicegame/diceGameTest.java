94cc2e4dcad8daf7d1e927130a9d5677de972169
David McClure <sokerballer@gmail.com>
2016-04-15 16:30:41 -0700

    Updating Junit tests.

--- src/dicegame/diceGameTest.java
+++ src/dicegame/diceGameTest.java
@@ -4,30 +4,45 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import dicegame.Game;
+
 public class diceGameTest {
 
 	
 	@Test
 	public void constructorTest()
 	{
-		@SuppressWarnings("unused")
-		Game test = new Game();
-	}
-	
-	
 
-	
-	
-	@Test
-	public void createPlayer(String name)
-	{
 		Game test = new Game();
 		
-		test.createPlayer("Tom");
-		
-		System.out.println("Names of players: " + test.getPlayer());
-		
+		assertNotNull(test);
+//		assertEquals(1, playerCount);
+//		assertEquals(0, numberOfStrokes);
+//		assertEquals(36, distanceRemaining);
+
 	}
+	
+//	@Test
+//	public void setNumberOfPlayers(int numberOfPlayers)
+	
+//	@Test
+//	public void getNumberOfPlayers()
+	
+//	@Test
+//	public void getPlayer()
+	
+	
+	
+//	@Test
+//	public void createPlayer(String name)
+//	{
+//		Game test = new Game();
+//		
+//		test.createPlayer("Tom");
+//		
+//		System.out.println("Names of players: " + test.getPlayer());
+//		
+//	}
 	
 
 	
@@ -36,19 +51,40 @@ public class diceGameTest {
 	{
 		Game test = new Game();
 		
-		int roll = test.roll();
-		System.out.println("Roll value should be between 1 and 6: " );
-		System.out.println("Roll value: " + roll);
+		  int high = 6;
+		  int low = 0;
+		  
+		  //Runs 100 attempts of random value assignment to ensure value is between 0 and 6
+		  for(int i = 0; i < 100; i++)
+		  {
+			  int roll = test.roll();
+			  assertTrue("Error, random is too high", high >= roll);
+			  assertTrue("Error, random is too low",  low  <= roll);
+			 //System.out.println("Test passed: " + roll + " is within " + high + " and " + low);
+		  }
+		  
 	}
 
 	@Test
-	public void hitTheBall(int roll)
+	public void hitTheBall()
 	{
+		
+		int rollValue = 0;
+		
 		Game test = new Game();
 		
-		int rollValue = test.roll();
+		//rolls random value between 0 and 6
+		rollValue = test.roll();
+		
+		//rolls 'rollValue' of dice and stores each of their values
 		int[] distance = test.hitTheBall(rollValue);
 		
-		System.out.print("Distance of first stroke: " + distance);
+		
+	 	//prints out distance of each stroke
+		for(int i = 0; i < rollValue; i++)
+ 		{
+			System.out.println("Distance: " + distance[i]);
+ 		}
+		
 	}
 }
