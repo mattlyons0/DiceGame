78973d437a3a491a7c514f7313dd8c1cab066231
Matt Lyons <matt@matt-lyons.net>
2016-04-28 13:27:11 -0700

    No more empty rolls on last turn
    Hacked past logic and only will display an empty roll if it is the first roll. This removes the 2-5 empty dice you could get beforehand.

--- src/dicegame/UI/GameControlsComponent.java
+++ src/dicegame/UI/GameControlsComponent.java
@@ -30,14 +30,15 @@ class GameControlsComponent extends JComponent implements ActionListener {
     private Game gameLogic;
     private GUI gui;
     private GameScoreboardComponent scoreboard;
-    
+
     private int[] rollValues;
+
     /**
      * Create a new GameControlsComponent and all its components.
      */
     public GameControlsComponent(GUI gui) {
         super();
-        
+
         this.gui = gui;
         this.gameLogic = gui.gameLogic;
 
@@ -67,7 +68,7 @@ class GameControlsComponent extends JComponent implements ActionListener {
 
     private void diceRollMultiplier() { //Display multiplier UI
         JButton multiplierRoll = new JButton("Roll Dice Multiplier");
-        multiplierRoll.setPreferredSize(new Dimension(180,76));
+        multiplierRoll.setPreferredSize(new Dimension(180, 76));
         multiplierRoll.setActionCommand("MultiplierRoll");
         multiplierRoll.addActionListener(this);
 
@@ -81,23 +82,27 @@ class GameControlsComponent extends JComponent implements ActionListener {
         dicePanel.removeAll();
 
         for (int number = 0; number < numDice; number++) {
-            JButton die = new JButton("Roll");
-            die.setPreferredSize(new Dimension(76,76));
-            die.setActionCommand("RollDie " + number);
-            die.addActionListener(this);
-            dicePanel.add(die);
+            if (number == 0 || rollValues[number] != 0) {
+                JButton die = new JButton("Roll");
+                die.setPreferredSize(new Dimension(76, 76));
+                die.setActionCommand("RollDie " + number);
+                die.addActionListener(this);
+                dicePanel.add(die);
+            } else{
+                rollValues[number]=-1;
+            }
         }
 
         dicePanel.revalidate();
     }
-    
-    private void updateTurn(){
+
+    private void updateTurn() {
         gameLogic.currentPlayer();
         System.out.println(Arrays.toString(gameLogic.getCurrentPlayer()));
-        
+
         int playerTurnIndex = gameLogic.getCurrentPlayer()[0];
         String player = gameLogic.getPlayer()[playerTurnIndex];
-        playerTurnLabel.setText(player+"'s Turn");
+        playerTurnLabel.setText(player + "'s Turn");
     }
 
     @Override
@@ -105,37 +110,38 @@ class GameControlsComponent extends JComponent implements ActionListener {
         if (event.getActionCommand().equals("MultiplierRoll")) {
             int multiplier = gameLogic.roll();
             rollValues = gameLogic.hitTheBall(multiplier); //Preroll values and display them as they are clicked
-            
+
             diceRoll(multiplier); //Update GUI with multiplier
         } else if (event.getActionCommand().startsWith("RollDie")) {
             int diceNum = Integer.parseInt(event.getActionCommand().split(" ")[1]);
-            
+
             //Show Dice Roll
-            
             JButton rollButton = (JButton) event.getSource();
             rollButton.setEnabled(false);
             rollButton.setText("");
-            rollButton.setIcon(new ImageIcon(getClass().getResource("/dicegame/Images/Dice" + rollValues[diceNum] 
-            		+ ".png")));
+            rollButton.setIcon(new ImageIcon(getClass().getResource("/dicegame/Images/Dice" + rollValues[diceNum]
+                    + ".png")));
             rollButton.removeActionListener(this);
-            if(scoreboard == null)
+            if (scoreboard == null) {
                 this.scoreboard = this.gui.gameplayPanel.scoreboardComp;
+            }
             scoreboard.hitBall(rollValues[diceNum]);
             gui.gameplayPanel.animationComp.repaint();
-            
+
             rollValues[diceNum] = -1; //Dice has been rolled and roll has been used
-            
+
             boolean allRolled = true;
-            for(int index = 0; index < rollValues.length; index++){
-                if(rollValues[index] != -1)
+            for (int index = 0; index < rollValues.length; index++) {
+                if (rollValues[index] != -1) {
                     allRolled = false;
+                }
             }
-            
-            if(allRolled){
+
+            if (allRolled) {
                 String buttonText = "Next Turn";
-                if(gameLogic.getDistanceFromHole() == 0 && gameLogic.getHoleIndex() == gui.gameplayPanel.TOTAL_HOLES)
+                if (gameLogic.getDistanceFromHole() == 0 && gameLogic.getHoleIndex() == gui.gameplayPanel.TOTAL_HOLES) {
                     buttonText = "End Game";
-                else if(gameLogic.getDistanceFromHole() == 0){
+                } else if (gameLogic.getDistanceFromHole() == 0) {
                     buttonText = "Next Hole";
                 }
                 JButton nextTurn = new JButton(buttonText);
@@ -143,11 +149,11 @@ class GameControlsComponent extends JComponent implements ActionListener {
                 nextTurn.addActionListener(this);
                 dicePanel.add(nextTurn);
             }
-        } else if (event.getActionCommand().equals("NextTurn")){
-            if(((JButton)event.getSource()).getText().equals("End Game")){
+        } else if (event.getActionCommand().equals("NextTurn")) {
+            if (((JButton) event.getSource()).getText().equals("End Game")) {
                 gui.showStartMenu();
                 return;
-            } else if(((JButton)event.getSource()).getText().equals("Next Hole")){
+            } else if (((JButton) event.getSource()).getText().equals("Next Hole")) {
                 gameLogic.nextHole();
                 gui.gameplayPanel.animationComp.repaint();
                 gui.gameplayPanel.scoreboardComp.newHole();
